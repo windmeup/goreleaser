bee61cdfb74d3070d47ddc9e14b36c2d057c03b1
windmeup <babamiya224@gmail.com>
2023-11-27 20:50:35 +0800

    fix ctx version

--- internal/pipe/semver/semver.go
+++ internal/pipe/semver/semver.go
@@ -26,6 +26,7 @@ func (Pipe) Run(ctx *context.Context) error {
 	if err != nil {
 		return fmt.Errorf("failed to parse tag '%s' as semver: %w", ctx.Git.CurrentTag, err)
 	}
+	ctx.Version = version
 	ctx.Semver = context.Semver{
 		Major:      sv.Major(),
 		Minor:      sv.Minor(),
